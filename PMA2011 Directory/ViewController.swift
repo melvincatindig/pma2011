00df5051cb50b1d6f79ff43c339684643d92621a
Chris Catindig <catindigmcm@gmail.com>
2017-08-20 20:45:47 +0800

    first commit ever

--- PMA2011 Directory/ViewController.swift
+++ PMA2011 Directory/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tView: UITableView!
+    
+    var cats = ["Chris","Lois","Matt"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tView.dataSource = self
+        tView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cats.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     
+        let cell = UITableViewCell ()
+        cell.textLabel?.text = cats [indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
